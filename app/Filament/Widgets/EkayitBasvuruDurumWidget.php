e927992bb17b64d07abcc123247299d9f038436e
Barış Yılmaz <baris@b3dp.com>
2026-04-16 22:32:06 +0300

    Front-Back-Entegrasyon-017

--- app/Filament/Widgets/EkayitBasvuruDurumWidget.php
+++ app/Filament/Widgets/EkayitBasvuruDurumWidget.php
@@ -5,13 +5,19 @@ namespace App\Filament\Widgets;
 use App\Filament\Resources\EkayitKayitResource;
 use App\Models\EkayitDonem;
 use App\Models\EkayitSinif;
-use Filament\Widgets\Widget;
+use Filament\Widgets\StatsOverviewWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
 
-class EkayitBasvuruDurumWidget extends Widget
+class EkayitBasvuruDurumWidget extends StatsOverviewWidget
 {
-    protected static string $view = 'filament.widgets.ekayit-basvuru-durum-widget';
+    protected ?string $heading = 'E-Kayıt Başvuru Durumları';
 
     protected int|string|array $columnSpan = 'full';
+
+    protected function getColumns(): int
+    {
+        return 3;
+    }
 
     public static function canView(): bool
     {
@@ -54,6 +60,34 @@ class EkayitBasvuruDurumWidget extends Widget
                     'reddedilen' => $kayitlar->where('durum', 'reddedildi')->count(),
                     'yedek' => $kayitlar->where('durum', 'yedek')->count(),
                 ];
+            })
+            ->all();
+    }
+
+    protected function getStats(): array
+    {
+        return collect($this->getSiniflerWithStats())
+            ->map(function (array $item): Stat {
+                /** @var EkayitSinif $sinif */
+                $sinif = $item['sinif'];
+
+                return Stat::make($sinif->ad, (string) $item['basvuru'])
+                    ->description(sprintf(
+                        'Bekleyen: %d | Onaylanan: %d | Red: %d | Yedek: %d',
+                        $item['bekleyen'],
+                        $item['onaylanan'],
+                        $item['reddedilen'],
+                        $item['yedek'],
+                    ))
+                    ->color($sinif->renk ?: 'primary')
+                    ->chart([
+                        $item['bekleyen'],
+                        $item['onaylanan'],
+                        $item['reddedilen'],
+                        $item['yedek'],
+                        $item['basvuru'],
+                    ])
+                    ->url($this->getEkayitListeUrl() . '?tableFilters[sinif_id][values][0]=' . $sinif->id);
             })
             ->all();
     }
