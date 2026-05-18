272f4ee5a110bd3f5d571ed1a74ae449856488ae
Barış Yılmaz <baris@b3dp.com>
2026-05-18 11:03:00 +0300

    fix: Hermes SMS entegrasyonu güncellendi ve gereksiz dosyalar temizlendi

--- resources/views/filament/pages/toplu-sms-sayfasi.blade.php
+++ resources/views/filament/pages/toplu-sms-sayfasi.blade.php
@@ -3,12 +3,13 @@
         {{ $this->form }}
 
         <div class="flex flex-wrap gap-3">
-            <x-filament::button color="gray" type="button" wire:click="onizle">
+            <x-filament::button color="gray" type="button" wire:click="onizle" wire:loading.attr="disabled" wire:target="onizle,gonder">
                 Önizle
             </x-filament::button>
 
-            <x-filament::button color="primary" type="submit">
-                Gönder
+            <x-filament::button color="primary" type="submit" wire:loading.attr="disabled" wire:loading.class="opacity-60 cursor-not-allowed" wire:target="gonder">
+                <span wire:loading.remove wire:target="gonder">Gönder</span>
+                <span wire:loading wire:target="gonder">Gönderiliyor...</span>
             </x-filament::button>
         </div>
     </form>
